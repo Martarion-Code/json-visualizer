176ff9140f6611e5b0fde4fe1020ff38eb30fd81
Martarion-Code <mharionmarterl@gmail.com>
2023-07-08 08:03:43 +0700

    Change default json or object that need to get rendered

--- src/store.jsx
+++ src/store.jsx
@@ -7,70 +7,77 @@ const useStore = create((set, get) => ({
   edges: [],
   viewport: { x: 0, y: 0, zoom: 0 },
   needToRenderJson: {
-    property1: "value1",
-    property2: {
-      subProperty1: "value2",
-      subProperty2: [1, 2, 3],
-    },
-    property3: [
+    websiteName: "Shopify",
+    categories: [
       {
-        name: "John",
-        age: 30,
-      },
-      {
-        name: "Jane",
-        age: 28,
-      },
-    ],
-    property4: {
-      nestedProperty1: {
-        nestedSubProperty: "value3",
-        nestedSubObject: {
-          subObjectProperty: "value5",
-        },
-      },
-      nestedProperty2: "value4",
-    },
-    property5: [
-      {
-        name: "Alice",
-        children: [
+        id: 1,
+        name: "Electronics",
+        products: [
           {
-            name: "Bob",
-            children: [
+            id: 101,
+            name: "Smartphone",
+            brand: "Apple",
+            price: 999.99,
+            reviews: [
               {
-                name: "Charlie",
-                children: [
-                  {
-                    name: "Dave",
-                    children: [],
-                  },
-                ],
+                id: 1001,
+                rating: 4.5,
+                comment: "Great phone!"
               },
-            ],
+              {
+                id: 1002,
+                rating: 5,
+                comment: "Best smartphone ever!"
+              }
+            ]
           },
-        ],
+          {
+            id: 102,
+            name: "Laptop",
+            brand: "Dell",
+            price: 1299.99,
+            reviews: [
+              {
+                id: 1003,
+                rating: 4,
+                comment: "Good performance, but heavy."
+              },
+              {
+                id: 1004,
+                rating: 5,
+                comment: "Excellent laptop for work."
+              }
+            ]
+          }
+        ]
       },
       {
-        name: "Eve",
-        children: [],
-      },
-    ],
-    property6: {
-      level1: {
-        level2: {
-          level3: {
-            level4: {
-              level5: {
-                level6: {
-                  level7: "value6",
-                },
-              },
-            },
+        id: 2,
+        name: "Clothing",
+        products: [
+          {
+            id: 201,
+            name: "T-Shirt",
+            brand: "Nike",
+            price: 29.99,
+            reviews: []
           },
-        },
-      },
-    },
+          {
+            id: 202,
+            name: "Jeans",
+            brand: "Levi's",
+            price: 59.99,
+            reviews: [
+              {
+                id: 1005,
+                rating: 4.5,
+                comment: "Comfortable and stylish."
+              }
+            ]
+          }
+        ]
+      }
+    ]
   },
   onNodesChange: (changes) => {
     set({ nodes: applyNodeChanges(changes, get().nodes) });
